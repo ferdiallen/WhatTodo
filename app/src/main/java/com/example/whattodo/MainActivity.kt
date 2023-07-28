3163750e4073d9cf01b999e4d7e518a8eb6ad51f
ferdiallen <ferdialif02@gmail.com>
2023-07-28 07:33:46 +0700

    Initial commit Add Room Dependency Add initial project structure

---
+++ app/src/main/java/com/example/whattodo/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.whattodo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.whattodo.presentation.home.HomeScreen
+import com.example.whattodo.ui.theme.WhatTodoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WhatTodoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen(onGoToDetailTask = {})
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/whattodo/presentation/home/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.whattodo.presentation.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    onGoToDetailTask: () -> Unit
+) {
+
+}
